97a126e35f83c42a8b659e8bc276fbcc55e40761
Ezcurra <matialeezcurra@gmail.com>
2017-02-08 21:27:13 -0300

    first implementation

---
+++ glink/glink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  glink
+//
+//  Created by Federicuelo on 8/2/17.
+//  Copyright © 2017 Glink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ glink/glink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  glink
+//
+//  Created by Federicuelo on 8/2/17.
+//  Copyright © 2017 Glink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
