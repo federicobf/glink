8fe4e17f50c4a124d7c1c3e8525a34bec8e45583
Ezcurra <matialeezcurra@gmail.com>
2017-04-25 17:55:25 -0300

    SuccessViewController

---
+++ glink/glink/SuccessViewController.h
@@ -0,0 +1,14 @@
+//
+//  SuccessViewController.h
+//  glink
+//
+//  Created by Federicuelo on 25/4/17.
+//  Copyright © 2017 Glink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuccessViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *overButton;
+
+@end
---
+++ glink/glink/SuccessViewController.m
@@ -0,0 +1,45 @@
+//
+//  SuccessViewController.m
+//  glink
+//
+//  Created by Federicuelo on 25/4/17.
+//  Copyright © 2017 Glink. All rights reserved.
+//
+
+#import "SuccessViewController.h"
+
+@interface SuccessViewController ()
+
+@end
+
+@implementation SuccessViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.overButton.layer.cornerRadius = 100;
+    self.overButton.layer.masksToBounds = YES;
+    
+  
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
