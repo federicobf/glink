3a7f65683c7839c7d8f5e8f9e7301d3e994b98c4
Ezcurra <matialeezcurra@gmail.com>
2017-07-01 15:53:53 -0300

    cambios a botones laterales

--- glink/glink/MedicalProfileTableViewCell.h
+++ glink/glink/MedicalProfileTableViewCell.h
@@ -12,5 +12,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *cellLabel;
 @property (weak, nonatomic) IBOutlet UITextField *cellTextField;
 @property (weak, nonatomic) IBOutlet UIView *line;
+@property (weak, nonatomic) IBOutlet UIButton *cellButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonWidth;
+@property (strong, nonatomic) NSIndexPath *indexPath;
+
 
 @end
